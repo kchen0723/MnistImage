a908ac9fbd613b46e1642fe0eb87259c756f50ed
kissinger <kchen@gmail.com>
2019-09-09 22:39:47 -0700

    add MnistImageInfo

---
+++ MnistImageInfo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MnistImage
+{
+    ////https://msdn.microsoft.com/zh-cn/magazine/dn745868.aspx
+    public class MnistImageInfo
+    {
+        public const int MNIST_WIDTH = 28;
+        public const int MNIST_HEIGHT = 28;
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Label { get; set; }
+        public byte[][] Pixels { get; set; }
+
+        public MnistImageInfo(byte[][] pixels, string label) 
+            : this(MNIST_WIDTH, MNIST_HEIGHT, pixels, label)
+        {
+        }
+
+        public MnistImageInfo(int width, int height, byte[][] pixels, string label)
+        {
+            this.Width = width;
+            this.Height = Height;
+            this.Pixels = pixels;
+            this.Label = label;
+        }
+
+        public string PixelString
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < this.Height; i++)
+                {
+                    for (int j = 0; i < this.Width; j++)
+                    {
+                        sb.Append(this.Pixels[i][j].ToString("X2"));
+                        sb.Append(" ");
+                    }
+                    sb.Append(Environment.NewLine);
+                }
+                return sb.ToString();
+            }
+        }
+
+        public Bitmap GetDefaultmap()
+        {
+            return GetBitmap(1);
+        }
+
+        public Bitmap GetBitmap(int magnification)
+        {
+            int totalWidth = this.Width * magnification;
+            int totalHeight = this.Height * magnification;
+            Bitmap result = new Bitmap(totalWidth, totalHeight);
+            Graphics graphic = Graphics.FromImage(result);
+            for (int i = 0; i < this.Width; i++)
+            {
+                for (int j = 0; j < this.Height; j++)
+                {
+                    int colorValue = 255 - this.Pixels[i][j];
+                    Color pixelColor = Color.FromArgb(colorValue, colorValue, colorValue);
+                    SolidBrush brush = new SolidBrush(pixelColor);
+                    graphic.FillRectangle(brush, j * magnification, i * magnification, magnification, magnification);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ MnistImageLoader.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MnistImage
+{
+    public class MnistImageLoader
+    {
+        public const int MNIST_IMAGE_NUMBER = 60000;
+        public static MnistImageInfo[] LoadMnistImages(string pixelFile, string lableFile)
+        {
+            MnistImageInfo[] result = new MnistImageInfo[MNIST_IMAGE_NUMBER];
+            byte[][] pixels = new byte[MnistImageInfo.MNIST_HEIGHT][];
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                pixels[i] = new byte[MnistImageInfo.MNIST_WIDTH];
+            }
+            using (FileStream fsPixels = new FileStream(pixelFile, FileMode.Open))
+            {
+                using (FileStream fsLables = new FileStream(lableFile, FileMode.Open))
+                {
+                    using (BinaryReader imageReader = new BinaryReader(fsPixels))
+                    {
+                        using (BinaryReader labelReader = new BinaryReader(fsLables))
+                        {
+                            int magicNumber = reverseBytes(imageReader.ReadInt32());
+                            int imageCount = reverseBytes(imageReader.ReadInt32());
+                            int rowCount = reverseBytes(imageReader.ReadInt32());
+                            int colcount = reverseBytes(imageReader.ReadInt32());
+                            int magicNumber2 = reverseBytes(labelReader.ReadInt32());
+                            int labelCount = reverseBytes(labelReader.ReadInt32());
+                            for (int i = 0; i < MNIST_IMAGE_NUMBER; i++)
+                            {
+                                for (int j = 0; j < MnistImageInfo.MNIST_HEIGHT; j++)
+                                {
+                                    for (int k = 0; k < MnistImageInfo.MNIST_WIDTH; k++)
+                                    {
+                                        byte b = imageReader.ReadByte();
+                                        pixels[i][j] = b;
+                                    }
+                                }
+                                Byte lbl = labelReader.ReadByte();
+                                MnistImageInfo image = new MnistImageInfo(pixels, lbl.ToString());
+                                result[i] = image;
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        private static int reverseBytes(int source)
+        {
+            byte[] intAsBytes = BitConverter.GetBytes(source);
+            Array.Reverse(intAsBytes);
+            return BitConverter.ToInt32(intAsBytes, 0);
+        }
+    }
+}
